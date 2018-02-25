465df1d03c7ab7e0c6e1332b1385c3b34e3bc4dd
Sunil <sunil9766@gmail.com>
2018-02-25 16:44:53 +0530

    Userprofile added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import Logo from './components/Logo';
 import Navigation from './components/Navigation';
+import UserProfile from './components/UserProfile';
 // css
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchTerm:"",
+      searchUrl:"",
+    };
+    this.apiKey = '87dfa1c669eea853da609d4968d294be';
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    
+  };
+
+  handleKeyUp(e){
+    if(e.key === 'Enter' && this.state.searchTerm !== ''){
+      let searchUrl = `search/multi?query=${this.state.searchTerm}&api_key=${this.apiKey}`;
+      this.setState({searchUrl:searchUrl});
+      console.log(searchUrl);
+    }
+  }
+
+  handleChange(e){
+    this.setState({searchTerm: e.target.value})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="Header">
           <Logo />
           <Navigation />
-
+          <div id="search" className="Search">
+            <input
+              type="search"
+              placeholder="Search for a title....."
+              onKeyUp={this.handleKeyUp}
+              onChange={this.handleChange}
+              value={this.state.searchTerm} />
+              
+          </div>
+          <UserProfile />
         </header>
       </div>
     );
---
+++ src/components/Hero.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class HeroButton extends React.Component {
+  render(){
+    return(
+      <a href='#' className='Button' data-primary={this.props.primary}>{this.props.text}</a>
+    );
+  }
+}
+
+
+export default class Hero extends React.Component {
+  render(){
+    return(
+      <div id="hero" className="Hero" style={{backgroundImage: 'url(https://images.alphacoders.com/633/633643.jpg)'}}>
+        <div className="content">
+          <img className="logo" src="http://www.returndates.com/backgrounds/narcos.logo.png" alt="narcos background" />
+          <h2>Season 2 now available</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id quam sapiente unde voluptatum alias vero debitis, magnam quis quod.</p>
+          <div className="button-wrapper">
+            <HeroButton primary={true} text="Watch now" />
+            <HeroButton primary={false} text="+ My list" />
+          </div>
+        </div>
+        <div className="overlay"></div>
+      </div>
+    );
+  }
+}
